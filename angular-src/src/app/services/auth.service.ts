be9767b44eb662c4f1a84d63478fca2449c664a7
Perry Craft <ljcraft12@gmail.com>
2017-03-13 14:03:41 -0400

    fixing database

--- angular-src/src/app/services/auth.service.ts
+++ angular-src/src/app/services/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+heimport { Injectable } from '@angular/core';
 import {Http, Headers} from '@angular/http';
 import 'rxjs/add/operator/map';
 import {tokenNotExpired} from 'angular2-jwt';
