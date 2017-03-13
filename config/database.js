39a7e272f80c99df5e9df03b7c052f78fc51ac1d
Perry Craft <ljcraft12@gmail.com>
2017-03-13 14:12:24 -0400

    database

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
   //database: 'mongodb://brad:brad@ds121190.mlab.com:21190/meanauthapp',   //prod
-  database: 'mongodb://:mean2:Help2008@ds129610.mlab.com:29610/meanapp',    //dev
+  database: 'mongodb://:meanapp:Help2008@ds129610.mlab.com:29610/meanapp',    //dev
   secret: 'yoursecret'
 }
