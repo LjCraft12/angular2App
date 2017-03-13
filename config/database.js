0b3a54ab86378d3534d9832136178e6559719523
Perry Craft <ljcraft12@gmail.com>
2017-03-13 14:17:42 -0400

    database

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
   //database: 'mongodb://brad:brad@ds121190.mlab.com:21190/meanauthapp',   //prod
-  database: 'mongodb://:meanapp:Help2008@ds129610.mlab.com:29610/meanapp',    //dev
+  database: 'mongodb://:heroku_bf75qfd3:Help2008!!@ds129610.mlab.com:29610/meanapp',    //dev
   secret: 'yoursecret'
 }
